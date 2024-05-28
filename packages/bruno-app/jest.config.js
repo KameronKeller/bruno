dea28326e1ba5d8a920f37a406822e8be766216d
KameronKeller <mrkameronkeller@gmail.com>
2024-05-28 10:47:28 -0700

    Add packages for seeing test coverage %

---
+++ packages/bruno-app/jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest'
+  }
+};
